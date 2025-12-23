210bb649215bb1c77d78668909dacf23e4e620a7
KimjeLassen <kimjelassen@yahoo.com>
2025-12-24 00:15:33 +0100

    SQL update

--- SQL/create_game_tag_assignment_table.sql
+++ SQL/create_game_tag_assignment_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE game_tag_assignment(
-game_id int NOT NULL,
-game_tag_id int NOT NULL,
+game_id int REFERENCES game(game_id),
+game_tag_id int REFERENCES game_tag(tag_id),
 PRIMARY KEY(game_id, game_tag_id)
 )
